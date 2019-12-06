d4d6aab989f952eecdf53be3505882f5cbf464e5
agateriver <xiepenguin@gmail.com>
2019-12-06 10:06:48 +0800

    Update xutils.go

--- xutils.go
+++ xutils.go
@@ -4,7 +4,6 @@ import (
 	"os"
 	"errors"
 	"fmt"
-	"github.com/deckarep/golang-set"
 )
 
 // IsFileExists if file exists, return true, otherwise false and err
@@ -17,15 +16,6 @@ func IsFileExists(file string) (bool, error) {
 		return false, errors.New(fmt.Sprintf("%v cannot be directory.", file))
 	}
 	return true, nil
-}
-
-// LenSet get a mapset.Set's length
-func Len(v mapset.Set) int {
-	i := 0
-	for range v.Iter() {
-		i++
-	}
-	return i
 }
 
 // Noop is a tricky for keep unused variables
